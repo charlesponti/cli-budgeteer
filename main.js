ae5e01cee27629e6ff6c3f6487728065ccadc74b
hackefeller <hackefeller@icloud.com>
2015-09-23 17:56:20 +0100

    [feaure] Add functionality to get unique list of categories

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 
 var _ = require('lodash');
 var transactions = require('./transactions');
+
+/**
+ * Unique list of categories
+ * @type {Array}
+ */
+var categories = _.unique(transactions.map(transaction => transaction.Category));
