2124e9248e53376df6861bc809f0cb563b8e9b17
hackefeller <hackefeller@icloud.com>
2015-09-25 10:58:12 +0100

    [refactor] Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,22 @@
 
 var _ = require('lodash');
 var transactions = require('./transactions');
+var budget = require('./budget');
+
+var transactionsByCategory = {};
 
 /**
  * Unique list of categories
  * @type {Array}
  */
 var categories = _.unique(transactions.map(transaction => transaction.Category));
+
+categories.forEach(category => {
+  var categoryTransactions = transactions.filter(t => t.Category === category);
+
+  transactionsByCategory[category] = categoryTransactions.reduce((prev, curr, i, arr) => {
+    return prev + parseInt(arr[i]['Amount']);
+  });
+});
+
+console.log(transactionsByCategory);
