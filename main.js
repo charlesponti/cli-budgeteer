12d44749821caeb2010be3c1e3df01840b0b8be8
hackefeller <hackefeller@icloud.com>
2015-09-28 12:49:44 +0100

    Use csvtojson to load transactions from csv file

--- main.js
+++ main.js
@@ -1,34 +1,49 @@
 
 var _ = require('lodash');
-var transactions = require('./transactions');
 var budget = require('./budget');
 var argv = require('yargs').argv;
 
 var transactionsByCategory = {};
 
-/**
- * Unique list of categories
- * @type {Array}
- */
-var categories = _.unique(transactions.map(transaction => transaction.Category));
-var totalSpend = 0;
+var Converter = require("csvtojson").Converter;
+var converter = new Converter({});
+var transactions;
 
-categories.forEach(category => {
-  var total = 0;
-  var categoryTransactions = transactions.filter(t => t.Category === category);
+var promise = new Promise(function(resolve, reject) {
+  //end_parsed will be emitted once parsing finished
+  converter.on("end_parsed", function (jsonArray) {
+    resolve(jsonArray);
+  });
+});
 
-  categoryTransactions.forEach(t => {
-    total += parseFloat(t['Amount']);
+//read from file
+require("fs").createReadStream("./september.csv").pipe(converter);
+
+promise.then(function(transactions) {
+  /**
+   * Unique list of categories
+   * @type {Array}
+   */
+  var categories = _.unique(transactions.map(transaction => transaction.Category));
+  var totalSpend = 0;
+
+  categories.forEach(category => {
+    var total = 0;
+    var categoryTransactions = transactions.filter(t => t.Category === category);
+
+    categoryTransactions.forEach(t => {
+      total += parseFloat(t['Amount']);
+    });
+
+    transactionsByCategory[category] = total;
+
+    // Add to total spend
+    totalSpend = parseFloat(totalSpend) + parseFloat(total);
   });
 
-  transactionsByCategory[category] = total;
+  _.keys(transactionsByCategory).forEach(k => {
+    console.log(`${k}: ${transactionsByCategory[k].toFixed(2)}`);
+  });
 
-  // Add to total spend
-  totalSpend = parseFloat(totalSpend) + parseFloat(total);
+  console.log(`Total Spend: ${totalSpend.toFixed(2)}`);
 });
-
-_.keys(transactionsByCategory).forEach(k => {
-  console.log(`${k}: ${transactionsByCategory[k].toFixed(2)}`);
-});
-
-console.log(`Total Spend: ${totalSpend.toFixed(2)}`);
