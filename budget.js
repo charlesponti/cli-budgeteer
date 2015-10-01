b852a371378207b76eb62e08ed7f12d262a74458
hackefeller <hackefeller@icloud.com>
2015-10-01 23:42:32 +0100

    [update] Add 'Living Expenses' to basic budget

--- budget.js
+++ budget.js
@@ -5,9 +5,10 @@ module.exports = {
     'Savings': 0.20,
     'Debt': 0.10,
     'Entertainment': 0.10,
-    'Shopping': 0.10,
+    'Shopping': 0.05,
     'Food': 0.10,
-    'Transportation': 0.10
+    'Transportation': 0.05,
+    'Living Expenses': 0.10
   },
   one: {
     Fixed: 0.50,
