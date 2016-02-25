e75732c738850048c4f396c38aa232cb286cd5ef
Charles Ponti <cj@ponti.io>
2016-02-25 11:50:03 +0000

    Fix ESLint errors

--- main.js
+++ main.js
@@ -1,25 +1,12 @@
-
-var _ = require('lodash');
-var fs = require('fs');
-var argv = require('yargs').argv;
-var Table = require('cli-table');
-var Converter = require('csvtojson').Converter;
-var converter = new Converter({});
-var transactionsByCategory = {};
-var transactions;
-var table;
-
-/**
- * Budget catgories and percentages
- * @type {object}
- */
-var budget = require('./budget');
-
-/**
- * Monthly income
- * @type {Number}
- */
-var income = parseFloat(argv.income);
+import {argv} from 'yargs'
+var _ = require('lodash')
+var fs = require('fs')
+var Table = require('cli-table')
+var Converter = require('csvtojson').Converter
+var converter = new Converter({})
+var transactionsByCategory = {}
+var table
+import create from './create'
 
 // Create Budget
 if (argv.create) {
@@ -29,76 +16,77 @@ if (argv.create) {
 if (argv.budget) {
   // Exit process if no file provided
   if (!argv.file) {
-    console.log('Please provide filename. Example: node main.js --file=foo.csv');
-    process.exit();
+    console.log('Please provide filename. Example: node main.js --file=foo.csv')
+    process.exit()
   }
 
   /**
    * Path to CSV file
    * @type {string}
    */
-  var file = argv.file;
+  var file = argv.file
 
   // Promise to be resolved after CSV file is parsed
-  var promise = new Promise(function(resolve, reject) {
-    converter.on("end_parsed", function (jsonArray) {
-      resolve(jsonArray);
-    });
-  });
+  var promise = new Promise((resolve, reject) => {
+    converter.on('end_parsed', (jsonArray) => resolve(jsonArray))
+  })
 
   // Convert CSV file
-  fs.createReadStream(`./${file}`).pipe(converter);
+  fs.createReadStream(`./${file}`).pipe(converter)
 
-  promise.then(function(transactions) {
+  promise.then((transactions) => {
     /**
      * Unique list of categories
      * @type {Array}
      */
-    var categories = _.unique(transactions.map(transaction => transaction.Category));
-    var totalSpend = 0;
-    var columns = ['Category', 'Amount', 'Percentage'];
-    var columnSizes = [25,25,25];
+    const categories = _.unique(
+      transactions.map((transaction) => transaction.Category)
+    )
+    var totalSpend = 0
+    var columns = ['Category', 'Amount', 'Percentage']
+    var columnSizes = [25, 25, 25]
 
     if (argv.income) {
-      columns.push('Percentage of Income');
-      columnSizes.push(25);
+      columns.push('Percentage of Income')
+      columnSizes.push(25)
     }
 
     // instantiate
     table = new Table({
       head: columns,
       colWidths: columnSizes
-    });
+    })
 
-    categories.forEach(category => {
-      var total = 0;
-      var categoryTransactions = transactions.filter(t => t.Category === category);
+    categories.forEach((category) => {
+      let total = 0
+      var categoryTransactions = (
+        transactions.filter((t) => t.Category === category)
+      )
 
-      categoryTransactions.forEach(t => {
-        total += parseFloat(t.Amount);
-      });
+      categoryTransactions.forEach((t) => {
+        total += parseFloat(t.Amount)
+      })
 
-      transactionsByCategory[category] = total;
+      transactionsByCategory[category] = total
 
       // Add to total spend
-      totalSpend = parseFloat(totalSpend) + parseFloat(total);
-    });
+      totalSpend = parseFloat(totalSpend) + parseFloat(total)
+    })
 
-    _.keys(transactionsByCategory).forEach(k => {
-      var amount = transactionsByCategory[k];
-      var percentage = ((amount/totalSpend) * 100).toFixed(2);
+    _.keys(transactionsByCategory).forEach((k) => {
+      var amount = transactionsByCategory[k]
+      var percentage = ((amount / totalSpend) * 100).toFixed(2)
 
       if (argv.income) {
-        var percentageOfIncome = ((amount/totalSpend) * 100).toFixed(2);
-        table.push([k, amount.toFixed(2), percentage, percentageOfIncome]);
+        var percentageOfIncome = ((amount / totalSpend) * 100).toFixed(2)
+        table.push([k, amount.toFixed(2), percentage, percentageOfIncome])
+      } else {
+        table.push([k, amount.toFixed(2), percentage])
       }
-      else {
-        table.push([k, amount.toFixed(2), percentage]);
-      }
-    });
+    })
 
-    console.log(table.toString());
+    console.log(table.toString())
 
-    console.log(`Total Spend: ${totalSpend.toFixed(2)}`);
-  });
+    console.log(`Total Spend: ${totalSpend.toFixed(2)}`)
+  })
 }
