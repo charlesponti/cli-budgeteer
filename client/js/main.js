a1886e2b4552290f81147d3c586ec8eb8b89f94b
Charles Ponti <cj@ponti.io>
2016-03-05 14:37:39 +0000

    Add logic to get transactions from api from the client

--- client/js/main.js
+++ client/js/main.js
@@ -1,1 +1,35 @@
 
+$.get('/api', (data) => {
+  window.accounts = {}
+
+  data.forEach((transaction) => {
+    var account = transaction.Account
+    var debit = transaction['Debit Amount']
+    var credit = transaction['Credit Amount']
+
+    if (Object.keys(window.accounts).indexOf(account) === -1) {
+      window.accounts[account] = {credit: 0, debit: 0, transactions: []}
+    }
+
+    if (typeof transaction['Debit Amount'] === 'number') {
+      window.accounts[account].debit += debit
+    }
+
+    if (typeof transaction['Credit Amount'] === 'number') {
+      window.accounts[account].credit += credit
+    }
+
+    transaction.date = new Date(transaction.Date)
+
+    window.accounts[account].transactions.push(transaction)
+  })
+
+  var accounts = Object.keys(window.accounts)
+
+  accounts.forEach((account) => {
+    account = window.accounts[account]
+    account.total = account.credit - account.debit
+  })
+
+  console.log(window.accounts)
+})
