fce36c542ac68a39cde6d38c3ef7d9d35f7847e8
Charles Ponti <cj@ponti.io>
2016-03-25 16:03:08 +0000

    Change income argument to yearly & monthly arguments

--- create.js
+++ create.js
@@ -10,7 +10,7 @@ import {categories} from './budget'
 
 module.exports = function () {
   // Exit process if no income provided
-  if (!argv.income) {
+  if (argv.montly || argv.yearly) {
     console.log('Please provide monthly income. Example: node main.js --income=3000.00')
     process.exit()
   }
@@ -19,22 +19,38 @@ module.exports = function () {
    * Monthly income
    * @type {Number}
    */
-  const income = parseFloat(argv.income)
+  let income
+
+  if (!isNaN(parseFloat(argv.monthly))) income = parseFloat(argv.monthly)
+  else if (!isNaN(parseFloat(argv.yearly))) income = parseFloat(argv.yearly)
 
   var budgetTable = new Table({
     head: ['Category', 'Percentage', 'Yearly', 'Monthly'],
     colWidths: [25, 25, 25, 25]
   })
 
-  _.keys(categories).forEach((key) => {
-    const value = categories[key].amount
-    budgetTable.push([
-      key,
-      (value * 100) + '%',
-      (income * value).toFixed(2),
-      ((income * value) / 12).toFixed(2)
-    ])
-  })
+  if (argv.monthly) {
+    _.keys(categories).forEach((key) => {
+      const value = categories[key].amount
+      budgetTable.push([
+        key,
+        (value * 100) + '%',
+        ((income * value) * 12).toFixed(2),
+        (income * value).toFixed(2)
+      ])
+    })
+  } else if (argv.yearly) {
+    _.keys(categories).forEach((key) => {
+      const value = categories[key].amount
+      budgetTable.push([
+        key,
+        (value * 100) + '%',
+        (income * value).toFixed(2),
+        ((income * value) / 12).toFixed(2)
+      ])
+    })
+  }
+
 
   console.log(budgetTable.toString())
 }
