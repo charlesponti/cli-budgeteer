0ad1175917f998c8106c6e666765a6dd676be317
🇵🇷 <cj@ponti.io>
2017-10-05 17:03:45 +0100

    refactor(type/Transaction): Move gql to .graphql file

--- src/types/Transaction.js
+++ src/types/Transaction.js
@@ -1,17 +1,5 @@
 const { GraphQLObjectType, GraphQLFloat, GraphQLString } = require('graphql')
 
-export const type = `
-  type Transaction {
-    id: ID!
-    account: String!
-    transferAccount: ID
-    description: String
-    category: String!
-    date: String!
-    amount: Int!
-  }
-`
-
 module.exports = new GraphQLObjectType({
   name: 'csvTransaction',
   description: 'Transaction from csv',
