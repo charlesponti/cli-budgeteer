a7bbdcd1aed7fe856274de0deffc2d23c93aa768
🇵🇷 <cj@ponti.io>
2017-10-07 20:23:32 +0100

    fix(data/index.js): Fix issue with numbers containing `,`

--- src/data/index.js
+++ src/data/index.js
@@ -2,9 +2,26 @@ const fs = require('fs')
 const path = require('path')
 const { Converter } = require('csvtojson')
 
-let TRANSACTIONS
-let ACCOUNTS
-let CATEGORIES
+/**
+ * @namespace transaction
+ * @property {string} Account
+ * @property {string} Tranfers
+ * @property {string} Category
+ * @property {string} Payee
+ * @property {string} Description
+ * @property {number} Amount
+ */
+
+/**
+ * @namespace Summary
+ * @property {number} balance
+ * @property {transaction[]} transactions
+ * @property {string[]} accounts
+ * @property {string[]} categories
+ */
+let Summary = {
+
+}
 
 const converter = new Converter({})
 const filePath = path.resolve(__dirname, './transactions.csv')
@@ -26,8 +43,7 @@ converter.fromFile(filePath, (err, transactions) => {
 
   const convertToTransactionType = t => ({
     ...t,
-    Date: new Date(t.Date),
-    Amount: parseFloat(t.Amount)
+    Date: new Date(t.Date)
   })
 
   /**
@@ -47,24 +63,33 @@ converter.fromFile(filePath, (err, transactions) => {
     transactions: []
   }
 
+  function addToTotal (a, b) {
+    const round = b => Math.round(b * 100) / 100
+    if (typeof b === 'string') return round(a + parseFloat(b.replace(',', '')))
+    else if (typeof b === 'number') return round(a + b)
+    return round(a)
+  }
+
   // Set value of TRANSACTIONS in outer scope and convert items to TransactionType
-  TRANSACTIONS = transactions.reduce(function (a, t) {
+  Summary = transactions.reduce(function (a, t) {
     t = convertToTransactionType(t)
 
     return {
       accounts: addIfDoesntExist(a.accounts, t.Account),
       categories: addIfDoesntExist(a.categories, t.Category),
-      balance: a.balance + t.Amount,
+      balance: addToTotal(a.balance, t.Amount),
       transactions: a.transactions.concat(t)
     }
   }, baseState)
+
+  console.log(Summary.balance)
 })
 
 module.exports = {
   getSummary () {
-    return TRANSACTIONS
+    return Summary
   },
   getTransactions () {
-    return TRANSACTIONS.transactions
+    return Summary.transactions
   }
 }
