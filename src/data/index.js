802687b2ce6e06f9148b59b530e0c9f1270df330
🇵🇷 <cj@ponti.io>
2017-11-01 11:57:00 +0000

    refactor(data/index): Use ENV variables

--- src/data/index.js
+++ src/data/index.js
@@ -1,16 +1,13 @@
 require('dotenv').config()
 const Sequelize = require('sequelize')
+const { DB, DB_LOGIN, DB_PASSWORD, DB_HOST, DB_DIALECT } = process.env
+const { eq } = Sequelize.Op
 
-const Conn = new Sequelize(
-  'relay',
-  'postgres',
-  'postgres',
-  {
-    dialect: 'postgres',
-    host: 'localhost',
-    operatorsAliases: false
-  }
-)
+const Conn = new Sequelize(DB, DB_LOGIN, DB_PASSWORD, {
+  dialect: DB_DIALECT,
+  host: DB_HOST,
+  operatorsAliases: { $eq: eq }
+})
 
 const Account = Conn.define('account', {
   name: { type: Sequelize.STRING, allowNull: false },
