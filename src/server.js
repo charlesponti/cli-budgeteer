5e1614b245f33ab76eedb3d5dc3900c6806d8806
Charles Ponti <cj@ponti.io>
2020-10-26 18:49:18 -0400

    (fix) Move PORT definition after dotenv.config()

--- src/server.js
+++ src/server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const dotenv = require("dotenv");
 const GraphQLHTTP = require("express-graphql");
 const app = express();
-const { PORT } = process.env;
 
 // Load environment variables
 dotenv.config();
@@ -12,6 +11,7 @@ const { GraphQLSchema } = require("graphql");
 const logger = require("./logger");
 const mutation = require("./mutations");
 const query = require("./queries");
+const { PORT } = process.env;
 
 app.use(
   "*",
@@ -28,7 +28,7 @@ app.use(
 if (require.main === module) {
   app.listen(PORT, function() {
     logger.info(
-      `🚀 ${process.env.NODE_ENV.toUpperCase()} GraphQL Server running @ port 3000`
+      `🚀 ${process.env.NODE_ENV.toUpperCase()} GraphQL Server running @ port ${PORT}`
     );
   });
 }
