53656928dcc9f6c59c10d83577e7c1aabf56398d
Charles Ponti <cj@ponti.io>
2020-10-27 00:06:04 -0400

    (hotfix) Use force for db sync

--- src/data/sync.js
+++ src/data/sync.js
@@ -11,7 +11,7 @@ const { sequelize } = require("./index");
 logger.info("🚜 Beginning database sync...");
 
 sequelize
-  .sync()
+  .sync({ force: true })
   .then(() => {
     logger.info("✅ Completed database sync!");
     process.exit();
